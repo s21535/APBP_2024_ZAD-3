04af8b30f152eb79c4b2d8a60952fd129c9e83d9
s21535 <s21535@pjwstk.edu.pl>
2024-04-26 21:39:51 +0200

    Animal Controller WIP

---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace WebApplication1.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public String Name { get; set; }
+    public String Description { get; set; }
+    public String Category { get; set; }
+    public String Area { get; set; }
+}
